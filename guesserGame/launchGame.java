b44abe76297ae6442c73e0be2e48ac358ba06177
ritwik2003 <ritwikjain2003@gmail.com>
2023-02-03 21:54:27 +0530

    added game launch file

---
+++ guesserGame/launchGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Guesser {
+    int guessNum;
+
+    guessingNumber() {
+        System.out.print("Guesser guess a number : ");
+        Scanner scan = new Scanner(System.in);
+    }
+}
+
+public class launchGame {
+    public static void main(String[] args) {
+
+    }
+}
