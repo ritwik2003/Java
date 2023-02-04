d6cd02b49ee83328caa7393e7001c33331efb861
ritwik2003 <ritwikjain2003@gmail.com>
2023-02-04 15:17:00 +0530

    added enhanced for loop

--- practice/enhancedForloop.java
+++ practice/enhancedForloop.java
@@ -1,5 +1,15 @@
 public class enhancedForloop {
     public static void main(String[] args) {
-
+        int nums[][] = {
+                { 1, 2, 3, 4 },
+                { 4, 5, 6, 7 },
+                { 8, 9, 10, 11 }
+        };
+        for (int n[] : nums) {
+            for (int b : n) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
     }
 }
