8d3cf05dfd1a24754b616866ef283ac72afa0d03
ritwik2003 <ritwikjain2003@gmail.com>
2023-02-19 16:12:04 +0530

    added string concatenation and string methods

---
+++ practice/stringConcatenation.java
@@ -0,0 +1,15 @@
+public class stringConcatenation {
+    public static void main(String[] args) {
+        // String s1 = new String("ritwik");
+        // s1 = s1.concat("jain");
+        // System.out.println(s1);
+
+        String s1 = "ritwik";
+        String s2 = s1.concat("jain");
+        String s3 = new String("ritwik");
+        s3 = s3.concat("jain");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
---
+++ practice/stringMethods.java
@@ -0,0 +1,13 @@
+public class stringMethods {
+    public static void main(String[] args) {
+        String str = "Ritwik Jain";
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.length());
+        System.out.println(str.charAt(8));
+        System.out.println(str.substring(2, 8));
+        System.out.println(str.indexOf("i"));
+        System.out.println(str.lastIndexOf("i"));
+    }
+}
