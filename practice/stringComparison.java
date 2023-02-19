e571403db093b71e3ec065d138fe5c6785d244c7
ritwik2003 <ritwikjain2003@gmail.com>
2023-02-19 14:22:06 +0530

    string comparison added

---
+++ practice/stringComparison.java
@@ -0,0 +1,8 @@
+public class stringComparison {
+    public static void main(String[] args) {
+        String s1 = "ritwik";
+        String s2 = new String("ritwik");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+}
