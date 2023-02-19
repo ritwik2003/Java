16efe3906e74e72881e746474b8b6da57a0df114
ritwik2003 <ritwikjain2003@gmail.com>
2023-02-19 14:25:42 +0530

    added equalsIgnoreCase

--- practice/stringComparison.java
+++ practice/stringComparison.java
@@ -2,7 +2,9 @@ public class stringComparison {
     public static void main(String[] args) {
         String s1 = "ritwik";
         String s2 = new String("ritwik");
+        String s3 = new String("RITWIK");
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
     }
 }
