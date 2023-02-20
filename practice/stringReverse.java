a2146a5141f8fa072cbee6971014fc19b06bd309
ritwik2003 <ritwikjain2003@gmail.com>
2023-02-20 15:55:25 +0530

    added strings reverse case 1 and 2

--- practice/stringReverse.java
+++ practice/stringReverse.java
@@ -1,10 +1,23 @@
 public class stringReverse {
     public static void main(String[] args) {
-        String s = "Hello";
+
+        // case 1
+        // String s = "Ritwik Jain";
+        // String rev = "";
+        // for (int i = s.length() - 1; i >= 0; i--) {
+        // rev = rev + s.charAt(i);
+        // }
+        // System.out.println("string before reversing : " + s);
+        // System.out.println("string after reversing : " + rev);
+
+        // case 2
+        String s = "Ritwik Jain";
         String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev = rev + s.charAt(i);
+        String arr[] = s.split(" ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            rev = rev + arr[i] + " ";
         }
-        System.out.println(rev);
+        System.out.println("string before reversing : " + s);
+        System.out.println("string after reversing : " + rev);
     }
 }
